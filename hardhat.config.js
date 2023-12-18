c791cb652e91b34e02db35def14992813d1be574
Denis <zzomrot.ne@gmail.com>
2023-12-18 22:01:46 +0000

    Bump oz, use networks from solidity-utils

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,21 +7,24 @@ require('hardhat-deploy');
 require('hardhat-gas-reporter');
 require('hardhat-tracer');
 require('dotenv').config();
+const { Networks, getNetwork } = require('@1inch/solidity-utils/hardhat-setup');
 
-const { networks } = require('./hardhat.networks');
+const { networks, etherscan } = (new Networks()).registerAll();
 
 module.exports = {
     solidity: {
-        version: '0.8.19',
+        version: '0.8.23',
         settings: {
             optimizer: {
                 enabled: true,
                 runs: 1000000,
             },
+            evmVersion: networks[getNetwork()]?.hardfork || 'shanghai',
             viaIR: true,
         },
     },
     networks,
+    etherscan,
     gasReporter: {
         enabled: true,
         currency: 'USD',
